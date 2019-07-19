265b8ffb2ac7002c1165607180ec1c303a4ea5e3
cmlee <bal.dongdongdong@gmail.com>
2019-07-19 23:58:05 +0900

    커스텀 서버 삭제

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /* eslint-disable */
 const path = require('path');
+const coreJs = require('core-js');
 
 module.exports = {
   webpack: (config) => {
+    config.entry = async () => {
+      const entries = await originalEntry();
+      if (entries['main.js'] && !entries['main.js'].includes(coreJs)
+      ) {
+        entries['main.js'].unshift(cof)
+      }
+
+      return entries
+    }
     config.resolve.modules.push(path.resolve('./'));
     config.node = { fs: 'empty' };
     config.module.rules.push({
